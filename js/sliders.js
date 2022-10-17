18ef31d1cbb048ca6e4d58d363bad48a7e5ef248
Oleg Sadchikov <71182254+KiditsGood@users.noreply.github.com>
2022-10-17 16:35:29 +0300

    carousels

--- js/sliders.js
+++ js/sliders.js
@@ -7,7 +7,7 @@ const mainSlider = new Swiper('.slider--wrap', {
 
 const popularSlider = new Swiper('.popular__slider-wrap', {
     spaceBetween: 60,
-    slidesPerView: 3,
+    // slidesPerView: 'auto',
     pagination: {
         el: ".popular__slider-pag",
         clickable: true,
@@ -15,5 +15,37 @@ const popularSlider = new Swiper('.popular__slider-wrap', {
     navigation: {
         prevEl: '.pop__prev',
         nextEl: '.pop__next'
-    }
+    },
+    loop: true,
+    slidesPerView: 3,
+})
+
+const carouselTop = new Swiper('.carousel-top', {
+    spaceBetween: 24,
+    loop: true,
+    slidesPerView: 'auto',
+    autoplay: {
+        enabled: true,
+        delay: 0,
+        disableOnInteraction: false,
+        reverseDirection: true
+    },
+    speed: 4000,
+})
+
+const carouselBottom = new Swiper('.carousel-bottom', {
+    loop: true,
+    loopedSlides: 55,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    autoplay: {
+        enabled: true,
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    speed: 4000,
+})
+
+const auctionSlider = new Swiper('.auctions__slider-wrap', {
+    direction: 'vertical'
 })
